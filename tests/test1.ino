befcefbf6256bcc30850ae57dc1e9b5891b25bce
Maartje Eyskens <meyskens@me.com>
2017-10-29 14:04:29 +0100

    pfff... I really need to learn the libs

--- tests/test1.ino
+++ tests/test1.ino
@@ -5,7 +5,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-    Serial.start(9600);
+    Serial.begin(9600);
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
 }
