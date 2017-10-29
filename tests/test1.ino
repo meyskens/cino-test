be6e690aa719d8e8b139479d6bca4095b736ea55
Maartje Eyskens <meyskens@me.com>
2017-10-29 17:25:31 +0100

    slow down blinky

--- tests/test1.ino
+++ tests/test1.ino
@@ -14,8 +14,8 @@ void setup() {
 void loop() {
     Serial.println("TESTS FOR LIFE");
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500);                       // wait for a second
+    delay(2000);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(500);                       // wait for a second
+    delay(2000);                       // wait for a second
     Serial.print("\a");
 }
