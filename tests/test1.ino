60468bae11e75f5a7b5c1069157b658d3479e0a1
Maartje Eyskens <meyskens@me.com>
2017-10-29 14:01:47 +0100

    add an end test signal

--- tests/test1.ino
+++ tests/test1.ino
@@ -5,6 +5,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+    Serial.setup(9600);
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -15,4 +16,5 @@ void loop() {
     delay(1000);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     delay(1000);                       // wait for a second
+    Serial.print("\a");
 }
