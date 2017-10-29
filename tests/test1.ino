8ab8e94b3ab1d7cd984157b9459102fdc2deee2a
Maartje Eyskens <meyskens@me.com>
2017-10-29 15:31:43 +0100

    GO BLINKY GO

--- tests/test1.ino
+++ tests/test1.ino
@@ -14,8 +14,8 @@ void setup() {
 void loop() {
     Serial.println("TESTS FOR LIFE");
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
+    delay(500);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+    delay(500);                       // wait for a second
     Serial.print("\a");
 }
