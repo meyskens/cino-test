77a891517e52e21fb62840edd5aaea2523b753a4
Maartje Eyskens <meyskens@me.com>
2017-10-30 17:13:01 +0100

    add test 2

---
+++ tests/test2/test2.ino
@@ -0,0 +1,21 @@
+#line 2 "test2.ino"
+#include <ArduinoUnit.h>
+
+test(correct) {
+  int x=1;
+  assertEqual(x,1);
+}
+
+test(incorrect) {
+  int x=1;
+  assertNotEqual(x,1);
+}
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial); // for the Arduino Leonardo/Micro only
+}
+
+void loop() {
+  Test::run();
+}
